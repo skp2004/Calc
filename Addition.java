3ef5c52fd2b7621f1cc574c569742f3002d26c28
shashwat patel <shashwatpatel04@gmail.com>
2025-12-25 14:49:03 +0530

    Subtration added

--- Addition.java
+++ Addition.java
@@ -1,10 +1,10 @@
 class Addition {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
+        int a = 20;
+        int b = 10;
 
-        int sum = a + b;
+        int sum = a - b;
 
-        System.out.println("Addition of two numbers is: " + sum);
+        System.out.println("Subb of two numbers is: " + sum);
     }
 }
